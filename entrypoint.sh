f29ab53eba082ead65bdc70d00be6b23309fff28
sinspired <ggmomo@gmail.com>
2026-05-31 13:04:55 +0800

    test: 调试错误

--- entrypoint.sh
+++ entrypoint.sh
@@ -332,6 +332,7 @@ run_apprise() {
     echo "📤 [${label}] Sending..."
 
     if apprise \
+        -vv \
         --title "${TITLE}" \
         --body "${body}" \
         --input-format "${fmt}" \
